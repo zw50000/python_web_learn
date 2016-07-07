63bfe9a56edbfd9fb0ed047272a400f02fd63bc0
zw50000 <1243526058@qq.com>
2016-07-07 17:10:05 +0800

    Delete test1.py

--- test1.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return render_template('./base.html', name=None)
-
-if __name__ == '__main__':
-    app.run()
